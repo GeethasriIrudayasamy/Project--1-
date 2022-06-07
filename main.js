137e3f15b4b089f360e9a813d4f6e65e1249c33e
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-06-07 20:26:23 +0530

    dom manipulation

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const ul=document.querySelector('.items');
-ul.children[2].style.backgroundColor='green';
-ul.children[2].style.color='white';
-ul.style.fontWeight = "900";
+const ul = document.querySelector(".items");
+ul.children[2].style.backgroundColor = "green";
+ul.children[2].style.color = "white";
+ul.style.fontWeight = "700";
+
+
+var li = document.getElementsByTagName("li");
+li[4].textContent = "Hello";
+var items = document.getElementsByClassName("list-group-item");
+items[4].style.backgroundColor = "Yellow";
