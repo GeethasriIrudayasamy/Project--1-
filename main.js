17d6dec71704cac2818673eaa663894f82430605
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-06-25 17:49:00 +0530

    new change

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ ul.children[2].style.backgroundColor = "green";
 ul.children[2].style.color = "white";
 ul.style.fontWeight = "700";
 
-
 var li = document.getElementsByTagName("li");
 li[4].textContent = "Hello";
 var items = document.getElementsByClassName("list-group-item");
